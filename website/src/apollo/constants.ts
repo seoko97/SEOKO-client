f3442c18b733b9c0575be7f0ad1fc81924c0b7a8
seokho10007 <sukho1007@naver.com>
2023-06-04 18:04:07 +0900

    feat: mergeitem 제거

--- website/src/apollo/constants.ts
+++ website/src/apollo/constants.ts
@@ -1,7 +1,5 @@
 import { InMemoryCacheConfig } from "@apollo/client";
 
-import { mergeItem } from "@/utils/apollo/mergeItem";
-
 const APOLLO_STATE_PROP_NAME = "__APOLLO_STATE__";
 
 const MEMORY_CACHE_OPTIONS: InMemoryCacheConfig = {
@@ -10,7 +8,6 @@ const MEMORY_CACHE_OPTIONS: InMemoryCacheConfig = {
       fields: {
         getPosts: {
           keyArgs: ["input", ["category", "tag", "limit", "isTemporary", "text"]],
-          merge: mergeItem,
         },
       },
     },
