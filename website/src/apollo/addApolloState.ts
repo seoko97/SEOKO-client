87b43ea3f7eae16e82b0f126b9e614e3501acc7b
seokho10007 <sukho1007@naver.com>
2023-06-04 18:05:03 +0900

    feat: page props에 cache를 저장하는 addApolloState 함수 추가 (#5)

---
+++ website/src/apollo/addApolloState.ts
@@ -0,0 +1,9 @@
+import { ApolloClient, NormalizedCacheObject } from "@apollo/client";
+
+import { APOLLO_STATE_PROP_NAME } from "./constants";
+
+export const addApolloState = (client: ApolloClient<NormalizedCacheObject>, pageProps: any) => {
+  if (pageProps?.props) pageProps.props[APOLLO_STATE_PROP_NAME] = client.cache.extract();
+
+  return pageProps;
+};
