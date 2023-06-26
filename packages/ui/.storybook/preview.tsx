ea1840e1754a853c541fddb04e4d3f575e16a757
seokho10007 <sukho1007@naver.com>
2023-06-26 21:54:02 +0900

    feat: storybook darkmode 적용 (#14)

--- packages/ui/.storybook/preview.tsx
+++ packages/ui/.storybook/preview.tsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import type { Preview } from "@storybook/react";
 import { ThemeProvider } from "@emotion/react";
 import { theme } from "@seoko/theme";
 import { GlobalStyle } from "@seoko/ui";
 
+import { useDarkMode } from "storybook-dark-mode";
+
 export const decorators = [
-  (Story) => (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Story />
-    </ThemeProvider>
-  ),
+  (Story) => {
+    const mode = useDarkMode() ? "dark" : "light";
+
+    useEffect(() => {
+      localStorage.setItem("theme", mode);
+      document.body.dataset.theme = mode;
+    }, [mode]);
+
+    return (
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Story />
+      </ThemeProvider>
+    );
+  },
 ];
 
 const preview: Preview = {
@@ -23,6 +34,7 @@ const preview: Preview = {
       },
     },
   },
+  decorators,
 };
 
 export default preview;
