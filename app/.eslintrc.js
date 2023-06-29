d45eb88947b99d4980195b51c99eeba37b66468a
seokho10007 <sukho1007@naver.com>
2023-06-29 22:26:20 +0900

    feat: storybook 초기 설정

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -1,9 +1,10 @@
 /** @type {import('eslint').Rule} */
 module.exports = {
   root: true,
-  extends: ["@seoko/eslint-config"],
+  extends: ["@seoko/eslint-config", "plugin:storybook/recommended"],
   rules: {
     "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": ["error"],
+    "react/no-unknown-property": ["error", { ignore: ["css"] }],
+    "@typescript-eslint/no-unused-vars": "warn",
   },
 };
