2ffbeb476066d01b7d769a7fd63dae5f846a493f
seokho10007 <sukho1007@naver.com>
2023-07-14 00:04:00 +0900

    feat: QueryClientProvider 적용

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Noto_Sans_KR } from "next/font/google";
 import { AppProps } from "next/app";
-import { GlobalStyle } from "@seoko/ui";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import { theme } from "@seoko/theme";
 import { ThemeProvider } from "@emotion/react";
 
 import AppLayout from "@components/ui/templates/AppLayout";
 import Header from "@components/ui/organisms/Header";
+import { GlobalStyle } from "@/styles/GlobalStyle";
 
 const notoSansKr = Noto_Sans_KR({
   subsets: ["latin"],
@@ -15,13 +18,18 @@ const notoSansKr = Noto_Sans_KR({
 });
 
 const SEOKO = ({ Component, pageProps }: AppProps) => {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <AppLayout className={notoSansKr.className}>
-        <Header />
-        <Component {...pageProps} />
-      </AppLayout>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <AppLayout className={notoSansKr.className}>
+          <Header />
+          <Component {...pageProps} />
+        </AppLayout>
+      </QueryClientProvider>
     </ThemeProvider>
   );
 };
