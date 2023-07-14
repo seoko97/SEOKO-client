1967e19facf5e715b5e7da02da140a68835f3119
seokho10007 <sukho1007@naver.com>
2023-07-14 16:47:57 +0900

    feat: React Query SSR Hydration 실행 (#23)

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import { Noto_Sans_KR } from "next/font/google";
 import { AppProps } from "next/app";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { DehydratedState, Hydrate, QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { theme } from "@seoko/theme";
 import { ThemeProvider } from "@emotion/react";
@@ -12,23 +12,29 @@ import AppLayout from "@components/ui/templates/AppLayout";
 import Header from "@components/ui/organisms/Header";
 import { GlobalStyle } from "@/styles/GlobalStyle";
 
+interface IProps extends AppProps {
+  dehydratedState: DehydratedState;
+}
+
 const notoSansKr = Noto_Sans_KR({
   subsets: ["latin"],
   weight: ["400", "500", "700"],
 });
 
-const SEOKO = ({ Component, pageProps }: AppProps) => {
+const SEOKO = ({ Component, pageProps, dehydratedState }: IProps) => {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <QueryClientProvider client={queryClient}>
-        <ReactQueryDevtools initialIsOpen={false} />
-        <AppLayout className={notoSansKr.className}>
-          <Header />
-          <Component {...pageProps} />
-        </AppLayout>
+        <Hydrate state={dehydratedState}>
+          <ReactQueryDevtools initialIsOpen={false} />
+          <AppLayout className={notoSansKr.className}>
+            <Header />
+            <Component {...pageProps} />
+          </AppLayout>
+        </Hydrate>
       </QueryClientProvider>
     </ThemeProvider>
   );
