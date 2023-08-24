e641c64b7fc3dcbfeac5983af5c138e2c4e29368
seokho10007 <sukho1007@naver.com>
2023-08-24 14:17:28 +0900

    feat: user에 대한 정보를 정의하는 상수 추가

--- src/utils/constant/user.ts
+++ src/utils/constant/user.ts
@@ -1,7 +1,32 @@
+import { MailIcon, InstagramIcon, GithubIcon, FacebookIcon } from "@components/icons";
+
 const USER_DETAIL = {
   username: "지석호",
   description:
     "프론트엔드 개발자를 희망하고 있는 지석호입니다. 개발이 취미이며 기록하는 것을 좋아합니다.",
 };
 
-export { USER_DETAIL };
+const USER_LINKS = [
+  {
+    name: "GitHub",
+    link: "https://github.com/seoko97",
+    Icon: GithubIcon,
+  },
+  {
+    name: "Mail",
+    link: "mailto:seokoji97@gmail.com",
+    Icon: MailIcon,
+  },
+  {
+    name: "Facebook",
+    link: "https://www.facebook.com/profile.php?id=100002987223791",
+    Icon: FacebookIcon,
+  },
+  {
+    name: "Instagram",
+    link: "https://www.instagram.com/sukho1007",
+    Icon: InstagramIcon,
+  },
+];
+
+export { USER_DETAIL, USER_LINKS };
