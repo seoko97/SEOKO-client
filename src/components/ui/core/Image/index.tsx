c8511e6ce6b0b8e52a1cedc7efb7a852dd89331e
seokho10007 <sukho1007@naver.com>
2023-08-29 23:27:36 +0900

    feat: 주석 제거

--- src/components/ui/core/Image/index.tsx
+++ src/components/ui/core/Image/index.tsx
@@ -6,24 +6,12 @@ const Image = ({ width, height, ...rest }: ImageProps) => {
   return (
     <NextImage
       {...rest}
+      className={`relative aspect-[130/100] h-auto max-w-full rounded-lg object-cover  ${rest.className}`}
       quality={100}
       width={rest.fill ? undefined : width ?? 1000}
       height={rest.fill ? undefined : height ?? 1000}
     />
   );
 };
-
-// const Img = styled(NextImage)`
-//   position: relative;
-
-//   max-width: 100%;
-//   height: auto;
-//   aspect-ratio: 130 / 100;
-
-//   border-radius: 1rem;
-
-//   object-fit: cover;
-//   border-radius: 10px;
-// `;
 
 export default Image;
