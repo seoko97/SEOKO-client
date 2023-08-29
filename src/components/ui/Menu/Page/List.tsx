fda33e56868921e09b13d468ff16068fcb7fc316
seokho10007 <sukho1007@naver.com>
2023-08-29 23:30:00 +0900

    feat: series 페이지로 이동할 수 있는 item 추가

--- src/components/ui/Menu/Page/List.tsx
+++ src/components/ui/Menu/Page/List.tsx
@@ -10,6 +10,7 @@ const PageMenu: React.FC<IProps> = ({ isSign }) => {
   return (
     <ul className="menu">
       <MenuItem href="/">HOME</MenuItem>
+      <MenuItem href="/series">SERIES</MenuItem>
       <MenuItem href="/project">PROJECT</MenuItem>
       <MenuItem href="/about">ABOUT</MenuItem>
       {!isSign && <MenuItem href="/signin">로그인</MenuItem>}
