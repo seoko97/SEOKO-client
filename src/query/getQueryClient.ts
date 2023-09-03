18659f7d11c76e5be49de0d952b46ff8ff2ee1cd
seokho10007 <sukho1007@naver.com>
2023-09-04 00:11:05 +0900

    feat: QueryClient staleTime 설정

--- src/query/getQueryClient.ts
+++ src/query/getQueryClient.ts
@@ -2,8 +2,14 @@ import { cache } from "react";
 
 import { QueryClient } from "@tanstack/react-query";
 
+const STALE_TIME = 1000 * 60 * 5;
+
 const getQueryClient = cache(
-  () => new QueryClient({ defaultOptions: { queries: { retry: false } } }),
+  () =>
+    new QueryClient({
+      defaultOptions: { queries: { retry: false, staleTime: STALE_TIME } },
+    }),
 );
 
+export { STALE_TIME };
 export default getQueryClient;
