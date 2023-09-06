0c7d6db1a945b28abd4cba73ae993154a7986204
seokho10007 <sukho1007@naver.com>
2023-09-06 17:47:33 +0900

    feat: 폴더 이동

---
+++ src/components/pages/Post/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import PostHeader from "@components/ui/Post/PostHeader";
+import PostContent from "@components/ui/Post/PostContent";
+import Hydrate from "@components/pages/Post/Hydrate";
+
+interface IProps {
+  params: {
+    nid: number;
+  };
+}
+
+const Post = ({ params }: IProps) => {
+  const nid = Number(params.nid);
+
+  return (
+    <section className="frame flex w-[theme(screens.xl.max)] flex-col items-center xl:w-full">
+      <Hydrate nid={nid}>
+        <PostHeader nid={nid} />
+        <PostContent nid={nid} />
+      </Hydrate>
+    </section>
+  );
+};
+
+export default Post;
