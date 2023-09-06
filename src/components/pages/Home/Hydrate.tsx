92c67585f790e28dc4efb45299fdf839866b6eaf
seokho10007 <sukho1007@naver.com>
2023-09-06 17:47:11 +0900

    feat: Hydrate 폴더 이동

---
+++ src/components/pages/Home/Hydrate.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import { Hydrate as RqHydrate, dehydrate } from "@tanstack/react-query";
+import { IGetPostsInput } from "@/types";
+import getQueryClient from "@/query/getQueryClient";
+import { getPosts } from "@/apis/post";
+
+interface IProps {
+  children: React.ReactNode;
+  params: IGetPostsInput;
+}
+
+const Hydrate = async ({ children, params = {} }: IProps) => {
+  const queryClient = getQueryClient();
+
+  await queryClient.prefetchInfiniteQuery({
+    queryKey: ["posts", params],
+    queryFn: () => getPosts(params),
+  });
+  const dehydratedState = dehydrate(queryClient);
+
+  return <RqHydrate state={dehydratedState}>{children}</RqHydrate>;
+};
+
+export default Hydrate;
