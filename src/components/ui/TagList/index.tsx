7e8eb81e7998978edc155b13338dc6df8d86edd5
seokho10007 <sukho1007@naver.com>
2023-09-09 01:35:15 +0900

    feat: tag list component 추가

---
+++ src/components/ui/TagList/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import Tag from "@components/ui/TagList/item";
+import { ITag } from "@/types";
+
+interface IProps {
+  className?: string;
+  tags: ITag[];
+  onClick: (e: React.MouseEvent<HTMLDivElement>) => void;
+}
+
+const TagList = ({ className = "", tags, onClick }: IProps) => {
+  return (
+    <div className={`flex w-full flex-row flex-wrap items-center gap-x-1 gap-y-2 ${className}`}>
+      {tags.map((tag) => (
+        <Tag key={tag._id} onClick={onClick}>
+          {tag.name}
+        </Tag>
+      ))}
+    </div>
+  );
+};
+
+export default TagList;
