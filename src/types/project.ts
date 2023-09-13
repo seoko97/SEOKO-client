e6dc7849f7f7a2ac49bc3d37fd50980b412a75ba
seokho10007 <sukho1007@naver.com>
2023-09-13 23:58:49 +0900

    feat: IProjectInput Type 변경

--- src/types/project.ts
+++ src/types/project.ts
@@ -1,13 +1,14 @@
 import { IBaseResponse } from "@/types/base";
 
 interface IProjectInput {
+  _id?: string;
   title: string;
   description: string;
   content: string;
   thumbnail: string;
   github: string;
   start: string;
-  end: string;
+  end: string | null;
 }
 
 type TProject = IProjectInput & IBaseResponse;
