543b0eb8593739b7301b018bf91d9f0f279bcdb0
seokho10007 <sukho1007@naver.com>
2023-09-14 01:14:41 +0900

    feat: project api 복수형으로 변경

--- src/apis/project.ts
+++ src/apis/project.ts
@@ -2,31 +2,31 @@ import { IProjectInput, TProject } from "@/types";
 import api from "@/apis";
 
 const getProject = async (nid: number) => {
-  const { data } = await api.get<TProject>(`/project/${nid}`);
+  const { data } = await api.get<TProject>(`/projects/${nid}`);
 
   return data;
 };
 
 const getProjects = async () => {
-  const { data } = await api.get<TProject[]>(`/project`);
+  const { data } = await api.get<TProject[]>(`/projects`);
 
   return data;
 };
 
 const createProject = async (input: IProjectInput) => {
-  const { data } = await api.post<TProject>(`/project`, input);
+  const { data } = await api.post<TProject>(`/projects`, input);
 
   return data;
 };
 
 const updateProject = async (nid: number, input: IProjectInput) => {
-  const { data } = await api.put<TProject>(`/project/${nid}`, input);
+  const { data } = await api.put<TProject>(`/projects/${nid}`, input);
 
   return data;
 };
 
 const deleteProject = async (nid: number) => {
-  const { data } = await api.delete<TProject>(`/project/${nid}`);
+  const { data } = await api.delete<TProject>(`/projects/${nid}`);
 
   return data;
 };
