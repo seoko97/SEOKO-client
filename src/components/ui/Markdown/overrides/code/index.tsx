b34ed88d273d0997ca204f387cdb5ef5ebe1cf65
seokho10007 <sukho1007@naver.com>
2023-09-19 00:40:28 +0900

    feat: prism light를 통해 코드 하이라이팅 적용
    - 기존 Prism 컴포넌트를 사용했을 때 모든 스타일을 불러오던 문제를 해결
    - prism light를 통해 필요한 스타일만 불러오도록 수정

--- src/components/ui/Markdown/overrides/code/index.tsx
+++ src/components/ui/Markdown/overrides/code/index.tsx
@@ -1,6 +1,23 @@
 import React from "react";
 
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { oneDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+import typescript from "react-syntax-highlighter/dist/esm/languages/prism/typescript";
+import jsx from "react-syntax-highlighter/dist/esm/languages/prism/tsx";
+import tsx from "react-syntax-highlighter/dist/esm/languages/prism/jsx";
+import json from "react-syntax-highlighter/dist/esm/languages/prism/json";
+import javascript from "react-syntax-highlighter/dist/esm/languages/prism/javascript";
+import css from "react-syntax-highlighter/dist/esm/languages/prism/css";
+import bash from "react-syntax-highlighter/dist/esm/languages/prism/bash";
+
+import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
+
+SyntaxHighlighter.registerLanguage("javascript", javascript);
+SyntaxHighlighter.registerLanguage("typescript", typescript);
+SyntaxHighlighter.registerLanguage("jsx", jsx);
+SyntaxHighlighter.registerLanguage("tsx", tsx);
+SyntaxHighlighter.registerLanguage("bash", bash);
+SyntaxHighlighter.registerLanguage("css", css);
+SyntaxHighlighter.registerLanguage("json", json);
 
 import InlineCode from "./inlineCode";
 
@@ -11,16 +28,22 @@ interface IProps extends React.HTMLAttributes<HTMLElement> {
 const code = ({ children, className = "", ...props }: IProps) => {
   const language = className.replace("lang-", "");
 
+  const isText = language === "txt" || language === "text";
+  const TXT_CLASS = isText
+    ? "!text-markdown1 !transition-[color,background-color] !bg-markdown"
+    : "";
+
   return language ? (
     <SyntaxHighlighter
       language={language}
-      showLineNumbers={language !== "txt"}
       startingLineNumber={1}
+      showLineNumbers={!isText}
+      showInlineLineNumbers={true}
+      style={oneDark}
       codeTagProps={{
-        className:
-          "text-sm !text-markdown1 !transition-[color] !drop-shadow-none !text-shadow-none",
+        className: "text-sm",
       }}
-      className={`relative w-full rounded-md !bg-markdown shadow-md !transition-[background-color]`}
+      className={`relative w-full rounded-md !p-4 shadow-md !transition-[background-color] ${TXT_CLASS}`}
     >
       {String(children).replace(/\n$/, "")}
     </SyntaxHighlighter>
