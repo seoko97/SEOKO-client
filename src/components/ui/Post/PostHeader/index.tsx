9daa8c147087ca4102840188f756fe4393679aee
seokho10007 <sukho1007@naver.com>
2023-09-18 21:59:11 +0900

    feat: post header에서 series 정보 호출 (#52)

--- src/components/ui/Post/PostHeader/index.tsx
+++ src/components/ui/Post/PostHeader/index.tsx
@@ -5,8 +5,10 @@ import React from "react";
 import { useRouter } from "next/navigation";
 
 import { useGetUserQuery } from "@hooks/query/user";
+import { useGetSeriesQuery } from "@hooks/query/series";
 import { useDeletePostMutation, useGetPostQuery } from "@hooks/query/post";
 import TagList from "@components/ui/TagList";
+import PostSeriesInfo from "@components/ui/PostSeriesInfo";
 import PostSubInfo from "@components/ui/Post/PostHeader/PostSubInfo";
 import Navigation from "@components/ui/Navigation";
 import Image from "@components/ui/core/Image";
@@ -17,13 +19,14 @@ interface IProps {
 
 const PostHeader = ({ nid }: IProps) => {
   const router = useRouter();
-  const { data } = useGetPostQuery(nid);
+  const { data: post } = useGetPostQuery(nid);
   const { data: username } = useGetUserQuery();
+  const { data: series } = useGetSeriesQuery(post?.series?.nid);
   const { mutate: deletePostMutate } = useDeletePostMutation(nid);
 
-  if (!data) return null;
+  if (!post) return null;
 
-  const { title, likeCount, viewCount, thumbnail, tags, createdAt } = data;
+  const { title, likeCount, viewCount, thumbnail, tags, createdAt } = post;
 
   const editPost = () => {
     router.push(`/write/post/${nid}`);
@@ -46,11 +49,12 @@ const PostHeader = ({ nid }: IProps) => {
   };
 
   return (
-    <div className="mt-8 flex w-[theme(screens.md.max)] flex-col items-center justify-center gap-5 break-all md:w-full">
-      <Image src={thumbnail} alt="post-thumbnail" />
+    <div className="mb-2 mt-4 flex w-[theme(screens.md.max)] flex-col items-center justify-center gap-5 break-all md:w-full">
+      <Image src={thumbnail} alt="post-thumbnail" priority />
       <h1 className="text-xl font-bold text-primary transition-[color]">{title}</h1>
       {tags.length > 0 && <TagList className="justify-center" tags={tags} onClick={onClickTag} />}
       <PostSubInfo viewCount={viewCount} likeCount={likeCount} createdAt={createdAt} />
+      {series && <PostSeriesInfo selectedPostNid={nid} series={series} />}
       {username && <Navigation onDelete={deletePost} onEdit={editPost} />}
     </div>
   );
