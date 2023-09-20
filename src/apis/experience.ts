321661d12c9889eb97783e997ab440ea55928020
seokho10007 <sukho1007@naver.com>
2023-09-21 01:58:24 +0900

    fix: url이 skill 이던 오류 수정

--- src/apis/experience.ts
+++ src/apis/experience.ts
@@ -1,26 +1,26 @@
-import { ICreateExperience, TUpdateExperience } from "@/types/experience";
+import { ICreateExperience, IExperience, IUpdateExperience } from "@/types/experience";
 import api from "@/apis";
 
 const getExperiences = async () => {
-  const { data } = await api.get("/skills");
+  const { data } = await api.get<IExperience[]>("/experiences");
 
   return data;
 };
 
 const createExperience = async (input: ICreateExperience) => {
-  const { data } = await api.post("/skills", input);
+  const { data } = await api.post("/experiences", input);
 
   return data;
 };
 
-const updateExperience = async (_id: string, input: TUpdateExperience) => {
-  const { data } = await api.put(`/skills/${_id}`, input);
+const updateExperience = async (_id: string, input: IUpdateExperience) => {
+  const { data } = await api.put(`/experiences/${_id}`, input);
 
   return data;
 };
 
 const deleteExperience = async (_id: string) => {
-  const { data } = await api.delete(`/skills/${_id}`);
+  const { data } = await api.delete(`/experiences/${_id}`);
 
   return data;
 };
