59a2d71e91ef9f0051f8c2438fb96af2dce6e0e2
seokho10007 <sukho1007@naver.com>
2023-09-23 01:05:35 +0900

    feat: page 별 metadata 추가 (#58)
    - 동적 라우팅 페이지에 대한 metadata 추가

--- src/app/project/[nid]/page.tsx
+++ src/app/project/[nid]/page.tsx
@@ -1,1 +1,46 @@
+import { Metadata } from "next";
+
+import { defaultOpenGraph, siteMetadata } from "@utils/constant/metadata";
+import { getProject } from "@/apis/project";
+
+interface IProps {
+  params: {
+    nid: number;
+  };
+}
+
+export const generateMetadata = async ({ params }: IProps): Promise<Metadata> => {
+  const nid = Number(params.nid);
+
+  if (isNaN(nid)) return {};
+
+  const project = await getProject(nid);
+
+  if (!project) return {};
+
+  const { title, description, thumbnail } = project;
+
+  const metadataTitle = `PROJECT [${title}]`;
+  const url = `${siteMetadata.siteUrl}/project/${nid}`;
+
+  const publishedTime = new Date(project.createdAt).toISOString();
+  const modifiedTime = new Date(project.updatedAt || project.createdAt).toISOString();
+
+  return {
+    title: metadataTitle,
+    description,
+    alternates: { canonical: url },
+    openGraph: {
+      ...defaultOpenGraph,
+      url,
+      type: "article",
+      title: metadataTitle,
+      description,
+      images: [{ url: thumbnail }],
+      publishedTime,
+      modifiedTime,
+    },
+  };
+};
+
 export { default } from "@components/pages/Project/[nid]";
