5aee7cd3d22c1ee8154010822026f28feac61d33
seokho10007 <sukho1007@naver.com>
2023-09-22 21:45:19 +0900

    feat: 기존 분리된 컴포넌트를 하나의 컴포넌트로 변경 (#56)

--- src/components/ui/SkillList/index.tsx
+++ src/components/ui/SkillList/index.tsx
@@ -1,6 +1,6 @@
 import React, { useMemo } from "react";
 
-import SkillItem from "@components/ui/SkillList/item";
+import SkillImage from "@components/ui/core/SkillImage";
 import { ISkill } from "@/types/skill";
 
 interface IProps {
@@ -12,17 +12,37 @@ interface IProps {
 const SkillList = ({ skills = [], onClick, type }: IProps) => {
   const isAdmin = useMemo(() => Boolean(onClick), [onClick]);
 
+  const CURSOR = isAdmin ? "cursor-pointer" : "";
+
+  const filteredSkills = useMemo(
+    () =>
+      skills.filter((skill) => {
+        return !!skill.description;
+      }),
+    [skills],
+  );
+
   return (
     <div className="flex w-full flex-col items-start gap-4 [&:not(:last-child)]:mb-12">
-      <h3 className="h-fit text-lg font-bold text-primary transition-[color]">{type}</h3>
+      <h3 className="h-fit text-xl font-bold text-primary transition-[color]">{type}</h3>
       <ul className="flex w-full flex-1 list-inside list-disc flex-col flex-wrap gap-4 marker:text-primary marker:transition-[color] sm:justify-around md:gap-6">
-        {skills.map((skill) => (
-          <SkillItem
-            key={skill._id + Math.random()}
-            skill={skill}
-            onClick={onClick}
-            className={isAdmin ? "cursor-pointer" : ""}
-          />
+        {filteredSkills.map((skill) => (
+          <li
+            key={skill._id}
+            onClick={isAdmin && onClick ? () => onClick(skill) : undefined}
+            className={`flex w-full items-center gap-4 text-primary transition-[color] md:flex-col md:items-start ${CURSOR}`}
+          >
+            <div className="flex min-w-max items-center gap-4">
+              <span className="text-2xl text-slate-600 transition-[color] dark:text-slate-300 md:hidden">
+                •
+              </span>
+              <SkillImage priority src={skill.icon} alt={skill.name} />
+              <h3 className="font-medium">{skill.name}</h3>
+            </div>
+            <p className="gap-4 whitespace-pre-line break-all text-slate-600 transition-[color] dark:text-slate-300">
+              {skill.description}
+            </p>
+          </li>
         ))}
       </ul>
     </div>
