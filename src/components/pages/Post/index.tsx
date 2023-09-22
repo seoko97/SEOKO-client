c700d56dfd6ef1f16d9aefe50225af08d84ee3b0
seokho10007 <sukho1007@naver.com>
2023-09-22 21:46:59 +0900

    feat: width가 정해진 screen size를 따라가도록 변경

--- src/components/pages/Post/index.tsx
+++ src/components/pages/Post/index.tsx
@@ -13,7 +13,7 @@ interface IProps {
 const Post = async ({ searchParams }: IProps) => {
   return (
     <main className="frame flex flex-col items-center justify-center">
-      <section className="3-16 flex w-[700px] items-center justify-center gap-7 px-0 py-16 text-primary md:w-full md:flex-col md:gap-4 md:px-0 md:pb-12 md:pt-8">
+      <section className="3-16 flex w-[theme(screens.md.max)] items-center justify-center gap-7 px-0 py-16 text-primary md:w-full md:flex-col md:gap-4 md:px-0 md:pb-12 md:pt-8">
         <Avatar width={140} height={140} />
         <div className="flex flex-1 flex-col gap-4 transition-[color]">
           <h3 className="text-xl font-medium">👨🏻‍💻 {USER_DETAIL.username}</h3>
