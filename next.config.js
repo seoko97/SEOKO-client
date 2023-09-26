1ce8ce2bee3d4580b1609690f90614a7f38c59a7
seokho10007 <sukho1007@naver.com>
2023-09-26 23:35:58 +0900

    feat: next bundle analyzer 적용

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   output: "standalone",
   images: {
     domains: ["image.toast.com"],
@@ -9,4 +15,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withBundleAnalyzer(nextConfig);
