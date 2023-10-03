16f9d776f5d889ce85ae0cd67d5223b15b237f60
seokho10007 <sukho1007@naver.com>
2023-10-03 23:56:24 +0900

    fix: 배포시 서버측 Date값과 클라이언트측 Date값이 달라 Hydrate error가 발생하는 문제 수정

--- src/components/ui/client/post/PostHeader/PostSubInfo.tsx
+++ src/components/ui/client/post/PostHeader/PostSubInfo.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { dateTimeParser } from "@utils/dateTimeParser";
+import DateTime from "@components/ui/core/DateTime";
 import { LikeIcon, ViewIcon } from "@components/icons";
 
 interface IProps {
@@ -12,9 +12,7 @@ interface IProps {
 const PostSubInfo = ({ viewCount, likeCount, createdAt }: IProps) => {
   return (
     <div className="flex flex-wrap items-center justify-start gap-4 text-sm">
-      <span className="font-normal text-slate-500 dark:text-slate-400">
-        {dateTimeParser(createdAt)}
-      </span>
+      <DateTime className="font-normal text-slate-500 dark:text-slate-400" date={createdAt} />
       <div className="flex items-center justify-center gap-1">
         <ViewIcon className="h-[1.2em] w-[1.2em] stroke-slate-500 dark:stroke-slate-400" />
         <span className="text-slate-500 dark:text-slate-400">{viewCount}</span>
