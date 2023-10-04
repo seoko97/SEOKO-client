111779f67419e05108ffcbbe99cb546cec8da48e
seokho10007 <sukho1007@naver.com>
2023-10-04 23:02:12 +0900

    feat: alert icon color 변경

--- src/components/ui/Markdown/overrides/alert.tsx
+++ src/components/ui/Markdown/overrides/alert.tsx
@@ -43,7 +43,7 @@ const Alert = ({ type = "info", children }: IProps) => {
       className={`${style} relative my-12 rounded-md border-l-2 px-5 py-3 transition-colors sm:border-l-4 [&>*:not(svg)]:my-4`}
     >
       <Icon
-        className={`${iconStyle} duration-default absolute -left-0.5 top-0 h-8 w-8 -translate-x-1/2 -translate-y-1/2 overflow-visible rounded-full border border-white bg-white fill-current p-0.5 transition-colors dark:border-gray-900 dark:bg-gray-900 sm:h-10 sm:w-10`}
+        className={`${iconStyle} duration-default absolute -left-0.5 top-0 h-8 w-8 -translate-x-1/2 -translate-y-1/2 overflow-visible rounded-full border border-[theme(backgroundColor.primary)] bg-primary p-0.5 transition-[background-color,border] sm:h-10 sm:w-10`}
       />
       {children}
     </div>
