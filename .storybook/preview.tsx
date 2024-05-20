3f191cd2cec9b0582fbc9f2786b1a13695b52b8b
seokho10007 <sukho1007@naver.com>
2024-05-20 22:31:21 +0900

    feat: react query 요청이 msw를 통해 더미 데이터를 받도록 설정

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,6 +1,13 @@
 import "@/styles/globals.css";
+import { Preview } from "@storybook/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { initialize as mswInit, mswLoader } from "msw-storybook-addon";
 
-const preview = {
+mswInit({ onUnhandledRequest: "warn" });
+
+const queryClient = new QueryClient();
+
+const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
@@ -10,6 +17,17 @@ const preview = {
       },
     },
   },
+  loaders: [mswLoader],
+
+  decorators: [
+    (Story) => {
+      return (
+        <QueryClientProvider client={queryClient}>
+          <Story />
+        </QueryClientProvider>
+      );
+    },
+  ],
 };
 
 export default preview;
