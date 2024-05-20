0564d1d93b0c35eba0bab9b11df0be9828fc53e3
seokho10007 <sukho1007@naver.com>
2024-05-20 22:29:27 +0900

    chore: storybook 파일 경로 변경

--- __stories__/components/Header.stories.tsx
+++ __stories__/components/Header.stories.tsx
@@ -2,6 +2,8 @@ import type { Meta, StoryObj } from "@storybook/react";
 
 import Header from "@components/ui/Header";
 
+import { handlers } from "../../__tests__/common/handler";
+
 const meta = {
   title: "Component/Header",
   component: Header,
@@ -14,4 +16,18 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {};
+export const Default: Story = {
+  parameters: {
+    msw: {
+      handlers: [handlers.getUserFailed],
+    },
+  },
+};
+
+export const IsLogged: Story = {
+  parameters: {
+    msw: {
+      handlers: [handlers.getUserSuccess],
+    },
+  },
+};
