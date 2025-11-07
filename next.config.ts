13af902ad3fb9b558dcb06e47d15991a7b0a4ef6
seoko97 <sukho1007@naver.com>
2025-11-07 21:14:26 +0900

    feat: next.config.ts에 이미지 설정 추가 및 최적화

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,21 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  reactStrictMode: true,
+  output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.toast.com",
+        pathname: "/**",
+      },
+    ],
+    minimumCacheTTL: 86400,
+    deviceSizes: [320, 480, 768, 1024, 1280],
+    imageSizes: [96, 128, 256, 384, 512, 768, 1024],
+    formats: ["image/avif", "image/webp"],
+  },
 };
 
 export default nextConfig;
