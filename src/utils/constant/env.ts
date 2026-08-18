ceed4ae40dc70b89bcd52e658f2f7245230a37c8
seoko97 <sukho1007@naver.com>
2026-08-18 23:15:21 +0900

    feat: Giscus 댓글 기능 추가

--- src/utils/constant/env.ts
+++ src/utils/constant/env.ts
@@ -5,5 +5,19 @@ const API_URL = process.env.NEXT_PUBLIC_API_URL as string;
 const HOST = process.env.HOST as string;
 const GOOGLE_SITE_VERIFICATION = isProd ? process.env.GOOGLE_SITE_VERIFICATION : "";
 const GA_TRACKING_ID = isProd ? process.env.NEXT_PUBLIC_GA_ID : "";
+const GISCUS = {
+  src: "https://giscus.app/client.js",
+  crossOrigin: "anonymous",
+  repo: process.env.NEXT_PUBLIC_GISUS_REPO as string,
+  repoId: process.env.NEXT_PUBLIC_GISUS_REPO_ID as string,
+  category: "Announcements",
+  categoryId: process.env.NEXT_PUBLIC_GISUS_CATEGORY_ID as string,
+  mapping: "pathname",
+  strict: "0",
+  reactionsEnabled: "0",
+  emitMetadata: "0",
+  inputPosition: "bottom",
+  lang: "ko",
+} as const;
 
-export { isProd, TOKEN_EXPIRED, API_URL, GOOGLE_SITE_VERIFICATION, HOST, GA_TRACKING_ID };
+export { isProd, TOKEN_EXPIRED, API_URL, GOOGLE_SITE_VERIFICATION, HOST, GA_TRACKING_ID, GISCUS };
