cacea3a3b53b7bb2eed291f5663c6f43ecfdad81
seoko97 <sukho1007@naver.com>
2026-08-21 23:43:02 +0900

    chore : Next.js 16 업그레이드 설정 적용

---
+++ next.config.ts
@@ -0,0 +1,27 @@
+import type { NextConfig } from "next";
+import withBundleAnalyzer from "@next/bundle-analyzer";
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  reactCompiler: true,
+  experimental: {
+    turbopackRustReactCompiler: true,
+  },
+  output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.toast.com",
+        port: "",
+      },
+    ],
+    minimumCacheTTL: 86400,
+    deviceSizes: [640, 768, 980, 1080, 1200, 1440, 1920],
+    qualities: [80, 90, 100],
+  },
+};
+
+export default withBundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+})(nextConfig);
