6ee7ef7ed7a4257f5d92e1e3aaeb08f0328eae7c
seoko97 <sukho1007@naver.com>
2026-08-26 20:17:38 +0900

    refactor : About 사전 조회 query key 적용

--- src/components/pages/About/Hydrate.tsx
+++ src/components/pages/About/Hydrate.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
+import { experienceQueryKeys, projectQueryKeys, skillQueryKeys } from "@utils/query/queryKeys";
 import getQueryClient from "@utils/query/getQueryClient";
 import { getSkills } from "@/apis/skill";
 import { getProjects } from "@/apis/project";
@@ -15,9 +16,9 @@ const Hydrate = async ({ children }: IProps) => {
   const queryClient = getQueryClient();
 
   await Promise.allSettled([
-    queryClient.query({ queryKey: ["projects"], queryFn: getProjects }),
-    queryClient.query({ queryKey: ["skills"], queryFn: getSkills }),
-    queryClient.query({ queryKey: ["experiences"], queryFn: getExperiences }),
+    queryClient.query({ queryKey: projectQueryKeys.all, queryFn: getProjects }),
+    queryClient.query({ queryKey: skillQueryKeys.all, queryFn: getSkills }),
+    queryClient.query({ queryKey: experienceQueryKeys.all, queryFn: getExperiences }),
   ]);
 
   const dehydrateState = dehydrate(queryClient);
