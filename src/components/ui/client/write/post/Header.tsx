bae8b874dacd1c2240ca7c63a60f5b1305f8a03f
seoko97 <sukho1007@naver.com>
2026-08-29 01:18:23 +0900

    refactor : 작성 폼 썸네일 컴포넌트 공통화

--- src/components/ui/client/write/post/Header.tsx
+++ src/components/ui/client/write/post/Header.tsx
@@ -1,17 +1,23 @@
-import React from "react";
-
+import Thumbnail from "@components/ui/client/write/Thumbnail";
 import TitleEditor from "@components/ui/client/write/post/Title";
-import ThumbnailEditor from "@components/ui/client/write/post/Thumbnail";
 import TagEditor from "@components/ui/client/write/post/Tag";
 import SeriesEditor from "@components/ui/client/write/post/Series";
+import { EImageType } from "@/types/base";
+import { usePostWriteContext } from "@/context/PostWriteContext";
 
 const PostHeader = () => {
+  const { dataRef, updateData } = usePostWriteContext();
+
   return (
     <header className="flex w-full flex-col gap-4">
       <TitleEditor />
       <TagEditor />
       <SeriesEditor />
-      <ThumbnailEditor />
+      <Thumbnail
+        defaultValue={dataRef.current.thumbnail}
+        setThumbnail={(thumbnail) => updateData("thumbnail", thumbnail)}
+        type={EImageType.POST}
+      />
     </header>
   );
 };
