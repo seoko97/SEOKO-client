13a05aaa7aa9f9982ece0e8281a84d67d5e92af0
seoko97 <sukho1007@naver.com>
2026-09-09 01:33:39 +0900

    refactor : 로그인 페이지 인증 확인 서버 전환

--- src/components/pages/Signin/index.tsx
+++ src/components/pages/Signin/index.tsx
@@ -1,8 +1,13 @@
-import React from "react";
+import { redirect } from "next/navigation";
 
 import SigninClient from "@components/pages/Signin/page.client";
+import { getUserOrNull } from "@/apis/user";
 
-const Signin = () => {
+const Signin = async () => {
+  const user = await getUserOrNull();
+
+  if (user) return redirect("/");
+
   return (
     <div className="frame flex h-[calc(100vh-16rem)] items-center justify-center text-primary">
       <SigninClient />
